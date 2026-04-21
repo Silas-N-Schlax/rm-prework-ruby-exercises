64cc8da60d724626c3c768d936cf0b068f00db99
SilasNS <silasschlax@gmail.com>
2026-04-21 10:58:08 -0400

    Completed Recusion exercise 2

--- recursion/2_contains/exercises/contains_exercises.rb
+++ recursion/2_contains/exercises/contains_exercises.rb
@@ -1,8 +1,7 @@
 def contains?(hash, search_value)
-  # Write a method that recursively searches for a value in a nested hash.
-  # It should return `true` if the object contains that value.
-  #
-  # Examples:
-  # contains?({ foo: { bar: "baz" } }, "baz") # true
-  # contains?({ foo: { bar: "baz" } }, "egg") # false
+  values = hash.values
+  return true if values.include?(search_value)
+
+  nested_hash = values.select { |value| value.is_a?(Hash) }
+  nested_hash.any? { |hash|  contains?(hash, search_value)}
 end
